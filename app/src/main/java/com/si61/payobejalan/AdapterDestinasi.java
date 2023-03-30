649db6aa5cfc4a15d2a6d64853c11af722b52aa7
Vincent <vincenthwang23@mhs.mdp.ac.id>
2023-03-30 17:29:07 +0700

    bisa hapus

--- app/src/main/java/com/si61/payobejalan/AdapterDestinasi.java
+++ app/src/main/java/com/si61/payobejalan/AdapterDestinasi.java
@@ -1,12 +1,15 @@
 package com.si61.payobejalan;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -14,10 +17,12 @@ import java.util.ArrayList;
 public class AdapterDestinasi extends  RecyclerView.Adapter<AdapterDestinasi.viewHolderDestinasi>{
 
     private Context ctx;
-    private ArrayList arrNama, arrAlamat, arrJam;
+    private ArrayList arrNama, arrAlamat, arrJam, arrId;
 
-    public AdapterDestinasi(Context ctx, ArrayList arrNama, ArrayList arrAlamat, ArrayList arrJam) {
+
+    public AdapterDestinasi(Context ctx, ArrayList arrId, ArrayList arrNama, ArrayList arrAlamat, ArrayList arrJam) {
         this.ctx = ctx;
+        this.arrId = arrId;
         this.arrNama = arrNama;
         this.arrAlamat = arrAlamat;
         this.arrJam = arrJam;
@@ -33,6 +38,7 @@ public class AdapterDestinasi extends  RecyclerView.Adapter<AdapterDestinasi.vie
 
     @Override
     public void onBindViewHolder(@NonNull viewHolderDestinasi holder, int position) {
+        holder.tvId.setText(arrId.get(position).toString());
         holder.tvNama.setText(arrNama.get(position).toString());
         holder.tvAlamat.setText(arrAlamat.get(position).toString());
         holder.tvJam.setText(arrJam.get(position).toString());
@@ -45,13 +51,51 @@ public class AdapterDestinasi extends  RecyclerView.Adapter<AdapterDestinasi.vie
     }
 
     public class viewHolderDestinasi extends RecyclerView.ViewHolder{
-        TextView tvNama, tvAlamat, tvJam;
+        TextView tvId, tvNama, tvAlamat, tvJam;
         public viewHolderDestinasi(@NonNull View itemView) {
             super(itemView);
-
+            tvId = itemView.findViewById(R.id.tv_id);
             tvNama = itemView.findViewById(R.id.tv_nama);
             tvAlamat = itemView.findViewById(R.id.tv_alamat);
             tvJam = itemView.findViewById(R.id.tv_jam);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Perintah Apa yang Akan Dilakukan?");
+                    pesan.setCancelable(true);
+
+
+                    pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MyDatabaseHelper myDB = new MyDatabaseHelper(ctx);
+                            long eks = myDB.hapusData(tvId.getText().toString());
+                            if(eks == -1){
+                                Toast.makeText(ctx, "Gagal Hapus Data", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(ctx, "Sukses Hapus Data", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                                ((MainActivity) ctx).onResume();
+                            }
+                        }
+                    });
+
+                    pesan.show();
+
+                    return false;
+                }
+            });
         }
     }
 }
